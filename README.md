745d094e9920a2e9ffd5469493ee7e121ad4a297
hjessa <you@example.com>
2019-06-02 21:39:42 +0200

    testy

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 "# Projekt" 
-"# Projekt" 
+"# Projekt"
--- index.php
+++ index.php
@@ -1,1 +1,51 @@
-<echo> lolsasdas
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="Stylesheet" href="style.css">
+<title>Specialist English Transaltor</title>
+</head>
+<body>
+    <div id="container">
+        <div id="banner">
+          <h1>TUTAJ BĘDZIE JAKIŚ BANER </h1>
+        </div>
+        <div class="clear"></div>
+          <div id="menu">
+              <ul>
+                <li><h2><a href="">TUTAJ</a><h2></li>
+                <li><h2><a href="">BĘDZIE</a><h2></li>
+                <li><h2><a href="">JAKIEŚ</a><h2></li>
+                <li><h2><a href="">TAM</a><h2></li>
+                <li><h2><a href="">MENU</a><h2></li>
+              </ul>
+          </div>
+          <div id="panel">
+            <form>
+              LOGIN:<br>
+              <input type="text" name="login" value="">
+              <br>
+              PASSWORD:<br>
+              <input type="text" name="password" value="">
+            </form>
+          </div>
+          <div class="clear"></div>
+          <div id="home">
+              Lorem ipsum dolor sit amet, est libris consulatu eu, has dicat clita facilis ad. Id sed velit epicurei, vim veritus oportere elaboraret at, semper cetero ad sit. Mei ut odio errem, no erat nominati sed. Ut mea duis epicurei. Vel nonumes vivendum necessitatibus et, legere tractatos delicatissimi no cum, est tibique denique signiferumque no.
+
+              Et vel graeco delenit vivendo, cu quem elitr detraxit nec. Eam ut nisl dolor definitiones, modus dissentiet est ea. His et impetus percipitur delicatissimi. Nec erroribus inciderint ad, mel ei cotidieque efficiantur, prima menandri qui at. Harum patrioque ut nam, quo case principes et, no bonorum habemus officiis vix. In est graeci ornatus mandamus, pri ne possim admodum postulant.
+
+              Qui te audiam eripuit signiferumque. Postea deleniti expetendis sit ex, eum decore delicata ex. Consulatu voluptatum referrentur id nec. Impedit mentitum comprehensam cum ex. Volumus pericula et mea, te cum sale commune antiopam, ex quo simul graeco.
+
+              Eos eu dicat graece. Vim quidam appellantur ne. An sit aperiri dissentiunt interpretaris, sea te facilis assueverit ullamcorper. Et quidam iriure mei, duo te erat ridens. Mea purto saperet moderatius ad, sonet soluta sed at.
+
+              Expetenda intellegebat qui an, fugit zril id mea, vix regione aliquid euripidis ea. Et vix suscipiantur interpretaris, unum legendos ut eam. Brute commune per ex. Te etiam gloriatur vix, probo summo solet cu nec. Ex vim harum virtute. No nam iusto scripta, mei te affert timeam concludaturque.
+              Lorem ipsum dolor sit amet, est libris consulatu eu, has dicat clita facilis ad. Id sed velit epicurei, vim veritus oportere elaboraret at, semper cetero ad sit. Mei ut odio errem, no erat nominati sed. Ut mea duis epicurei. Vel nonumes vivendum necessitatibus et, legere tractatos delicatissimi no cum, est tibique denique signiferumque no. Et vel graeco delenit vivendo, cu quem elitr detraxit nec. Eam ut nisl dolor definitiones, modus dissentiet est ea. His et impetus percipitur delicatissimi. Nec erroribus inciderint ad, mel ei cotidieque efficiantur, prima menandri qui at. Harum patrioque ut nam, quo case principes et, no bonorum habemus officiis vix. In est graeci ornatus mandamus, pri ne possim admodum postulant. Qui te audiam eripuit signiferumque. Postea deleniti expetendis sit ex, eum decore delicata ex. Consulatu voluptatum referrentur id nec. Impedit mentitum comprehensam cum ex. Volumus pericula et mea, te cum sale commune antiopam, ex quo simul graeco. Eos eu dicat graece. Vim quidam appellantur ne. An sit aperiri dissentiunt interpretaris, sea te facilis assueverit ullamcorper. Et quidam iriure mei, duo te erat ridens. Mea purto saperet moderatius ad, sonet soluta sed at. Expetenda intellegebat qui an, fugit zril id mea, vix regione aliquid euripidis ea. Et vix suscipiantur interpretaris, unum legendos ut eam. Brute commune per ex. Te etiam gloriatur vix, probo summo solet cu nec. Ex vim harum virtute. No nam iusto scripta, mei te affert timeam concludaturque.Lorem ipsum dolor sit amet, est libris consulatu eu, has dicat clita facilis ad. Id sed velit epicurei, vim veritus oportere elaboraret at, semper cetero ad sit. Mei ut odio errem, no erat nominati sed. Ut mea duis epicurei. Vel nonumes vivendum necessitatibus et, legere tractatos delicatissimi no cum, est tibique denique signiferumque no. Et vel graeco delenit vivendo, cu quem elitr detraxit nec. Eam ut nisl dolor definitiones, modus dissentiet est ea. His et impetus percipitur delicatissimi. Nec erroribus inciderint ad, mel ei cotidieque efficiantur, prima menandri qui at. Harum patrioque ut nam, quo case principes et, no bonorum habemus officiis vix. In est graeci ornatus mandamus, pri ne possim admodum postulant. Qui te audiam eripuit signiferumque. Postea deleniti expetendis sit ex, eum decore delicata ex. Consulatu voluptatum referrentur id nec. Impedit mentitum comprehensam cum ex. Volumus pericula et mea, te cum sale commune antiopam, ex quo simul graeco. Eos eu dicat graece. Vim quidam appellantur ne. An sit aperiri dissentiunt interpretaris, sea te facilis assueverit ullamcorper. Et quidam iriure mei, duo te erat ridens. Mea purto saperet moderatius ad, sonet soluta sed at. Expetenda intellegebat qui an, fugit zril id mea, vix regione aliquid euripidis ea. Et vix suscipiantur interpretaris, unum legendos ut eam. Brute commune per ex. Te etiam gloriatur vix, probo summo solet cu nec. Ex vim harum virtute. No nam iusto scripta, mei te affert timeam concludaturque.Lorem ipsum dolor sit amet, est libris consulatu eu, has dicat clita facilis ad. Id sed velit epicurei, vim veritus oportere elaboraret at, semper cetero ad sit. Mei ut odio errem, no erat nominati sed. Ut mea duis epicurei. Vel nonumes vivendum necessitatibus et, legere tractatos delicatissimi no cum, est tibique denique signiferumque no. Et vel graeco delenit vivendo, cu quem elitr detraxit nec. Eam ut nisl dolor definitiones, modus dissentiet est ea. His et impetus percipitur delicatissimi. Nec erroribus inciderint ad, mel ei cotidieque efficiantur, prima menandri qui at. Harum patrioque ut nam, quo case principes et, no bonorum habemus officiis vix. In est graeci ornatus mandamus, pri ne possim admodum postulant. Qui te audiam eripuit signiferumque. Postea deleniti expetendis sit ex, eum decore delicata ex. Consulatu voluptatum referrentur id nec. Impedit mentitum comprehensam cum ex. Volumus pericula et mea, te cum sale commune antiopam, ex quo simul graeco. Eos eu dicat graece. Vim quidam appellantur ne. An sit aperiri dissentiunt interpretaris, sea te facilis assueverit ullamcorper. Et quidam iriure mei, duo te erat ridens. Mea purto saperet moderatius ad, sonet soluta sed at. Expetenda intellegebat qui an, fugit zril id mea, vix regione aliquid euripidis ea. Et vix suscipiantur interpretaris, unum legendos ut eam. Brute commune per ex. Te etiam gloriatur vix, probo summo solet cu nec. Ex vim harum virtute. No nam iusto scripta, mei te affert timeam concludaturque.
+          </div>
+          <div id="footer">
+            <h3>TUTAJ BĘDZIE STOPKA</h3>
+          </div>
+
+    </div>
+</body>
+</html>
